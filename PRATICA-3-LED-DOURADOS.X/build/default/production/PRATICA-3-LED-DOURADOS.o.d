8e23d74e3c9606f2af7743f30fb1117a2b8c9bba
Ozias Junior <76022880+OziasJr@users.noreply.github.com>
2024-05-11 10:01:16 -0400

    Add files via upload

---
+++ PRATICA-3-LED-DOURADOS.X/build/default/production/PRATICA-3-LED-DOURADOS.o.d
@@ -0,0 +1,3 @@
+build/default/production/PRATICA-3-LED-DOURADOS.o:  \
+	C:/Users/ozias/OneDrive/Documentos/Microcontroladores\ e\ Sistemas\ Embarcados\ -\ 2024S1/ATIVIDADES\ EM\ SALA\ -\ PRÁTICA/PRATICA-3-LED-DOURADOS-16F84A/PRATICA-3-LED-DOURADOS.X/PRATICA-3-LED-DOURADOS.asm  \
+
---
+++ PRATICA-3-LED-DOURADOS.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=PRATICA-3-LED-DOURADOS.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/PRATICA-3-LED-DOURADOS.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=pratica-3-led-dourados.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pratica-3-led-dourados.x.tar
